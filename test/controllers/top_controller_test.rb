ceb883d3e3eefbb8d49c05de250ba4e7ffbb236f
甲斐誉人 <kaiikohu3017@gmail.com>
2020-01-09 23:33:41 +0900

    change testcode

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
+  =begin
   test "should get index" do
     get top_index_url
     assert_response :success
   end
+  =end
 
 end
