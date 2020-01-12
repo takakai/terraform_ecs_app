c9df9a7f784de5afda1d729fa3028c79786a1839
甲斐誉人 <kaiikohu3017@gmail.com>
2020-01-12 11:50:43 +0900

    back root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'top#index'
-  resources :health_check, only: [:index]
+  get 'top/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
